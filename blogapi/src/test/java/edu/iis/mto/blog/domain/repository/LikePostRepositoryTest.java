1f0de4a62a9c61260f1390df0b0f3370eb4bbacb
gorazdnina <ninagorazd@gmail.com>
2021-05-28 14:12:42 +0200

    Added shouldModifySavedPost

--- blogapi/src/test/java/edu/iis/mto/blog/domain/repository/LikePostRepositoryTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/repository/LikePostRepositoryTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 
 @DataJpaTest
@@ -22,6 +23,7 @@ public class LikePostRepositoryTest {
     private LikePostRepository repository;
 
     private BlogPost blogPost;
+    private BlogPost blogPost2;
     private User user;
 
     @BeforeEach
@@ -32,6 +34,10 @@ public class LikePostRepositoryTest {
 
         blogPost = new BlogPost();
         blogPost.setEntry("entry");
+        blogPost.setUser(user);
+
+        blogPost2 = new BlogPost();
+        blogPost2.setEntry("entry2");
         blogPost.setUser(user);
     }
 
@@ -47,4 +53,25 @@ public class LikePostRepositoryTest {
 
         assertThat(savedLikePost.getId(), notNullValue() );
     }
+
+    @Test
+    void shouldModifySavedPost() {
+        entityManager.persist(user);
+        entityManager.persist(blogPost);
+
+        LikePost likePost = new LikePost();
+        likePost.setUser(user);
+        likePost.setPost(blogPost);
+        LikePost savedLikePost = repository.save(likePost);
+
+        LikePost likePost2 = new LikePost();
+        likePost2.setPost(blogPost2);
+        likePost2.setUser(user);
+        likePost2.setId(savedLikePost.getId());
+        LikePost savedSecondLikePost = repository.save(likePost2);
+
+        assertThat(savedSecondLikePost.getPost(), equalTo(savedLikePost.getPost()) );
+    }
+
+
 }
